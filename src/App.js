8f60a4e6aa36c03a72f9cbbd18b417438e78843a
[franck59840] <[depoorter.franck@gmail.com]>
2021-11-24 15:59:49 +0100

    add test hello world

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import './components/SlideBar/index.css'
+import './components/SlideBar/index.css';
 import SliderBar from './components/SlideBar';
 
 function App() {
   return (
     <div>
+      <h1>Hello world</h1>
       <SliderBar />
     </div>
   );
